6df541b7883800c412d58af8e847c7d53698ad15
Steven Judge <stevenjudge@Stevens-MacBook-Pro.local>
2025-05-10 14:25:10 +0300

    Clean up project structure and update configurations

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  poweredByHeader: false,
+  generateEtags: true,
+  compress: true,
+  productionBrowserSourceMaps: false,
 }
 
 module.exports = nextConfig 
